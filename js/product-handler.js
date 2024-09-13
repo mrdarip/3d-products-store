46255120db7c1b93abcd2f983396c5279c202afc
mrdarip <darioelas2005@gmail.com>
2024-09-14 01:12:35 +0200

    implemented unblurying images

--- js/product-handler.js
+++ js/product-handler.js
@@ -4,4 +4,12 @@ products.forEach((product) => {
   HQsrc = product.getAttribute("high-quality");
   LQsrc = product.getAttribute("low-quality");
   product.style.backgroundImage = "url(" + LQsrc + ")";
+
+  product.addEventListener("mouseover", () => {
+    product.style.backgroundImage = "url(" + HQsrc + ")";
+  });
+
+  product.addEventListener("mouseout", () => {
+    product.style.backgroundImage = "url(" + LQsrc + ")";
+  });
 });
