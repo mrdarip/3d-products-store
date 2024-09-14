a2530c937c4b6285b2265572a2f0c4c2cfb0b22a
mrdarip <darioelas2005@gmail.com>
2024-09-14 16:02:05 +0200

    styled page with fonts and implemented preloading high quality images

--- js/product-handler.js
+++ js/product-handler.js
@@ -1,18 +1,22 @@
 products = document.querySelectorAll(".product-preview");
 
 products.forEach((product) => {
-
-    console.log("hi");
-
   let HQsrc = product.getAttribute("high-quality");
   let LQsrc = product.getAttribute("low-quality");
+  preloadImage(HQsrc);
+  
   product.style.backgroundImage = "url(" + LQsrc + ")";
+
 
   product.addEventListener("mouseover", () => {
     product.style.backgroundImage = "url(" + HQsrc + ")";
   });
-
   product.addEventListener("mouseout", () => {
     product.style.backgroundImage = "url(" + LQsrc + ")";
   });
 });
+
+function preloadImage(url) {
+  var img=new Image();
+  img.src=url;
+}
